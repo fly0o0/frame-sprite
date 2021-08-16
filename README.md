11d47afd36de9b3b3994692d7eb8f91d8a7cde36
ygy <ygy@duiba.com.cn>
2021-08-16 15:25:26 +0800

    feat: init

--- README.md
+++ README.md
@@ -1,1 +1,40 @@
 # frame-sprite
+
+这是一款帧动画合成工具。
+
+通过该插件，可以将目录下的图片快速合成一张雪碧图，且输出你可能会需要的css文件。当前支持四种合成模式，后期会拓展更多需要的功能。
+
+## 使用
+
+```js
+    // 本地安装使用
+    npm install frame-sprite -D
+    npx frame-sprite -i test/imgs -o test/dist -m 0
+```
+参数说明
+-i 源文件输入目录（相对于命令执行的目录），默认值：img
+-o 雪碧图输出目录（相对于命令执行的目录），默认值：和-i一致
+-m 目前支持四种模式（0 1 2 3），对应生成的css文件，默认值：0
+
+## 范例
+
+模式3：
+
+![image.png](https://yun.dui88.com/tuia/cdn/readme/sprite2-3.png)
+
+
+模式2：
+
+![image.png](https://yun.dui88.com/tuia/cdn/readme/sprite3-2.png)
+
+
+模式1：
+
+![image.png](https://yun.dui88.com/tuia/cdn/readme/sprite1-6.png)
+
+
+模式0：
+
+![image.png](https://yun.dui88.com/tuia/cdn/readme/6_1-small.jpg)
+
+
---
+++ src/index.js
@@ -0,0 +1,176 @@
+#!/usr/bin/env node
+
+const program = require('commander');
+const nodeImages = require('images');
+const path = require('path');
+const fs = require('fs');
+const config = require('../package.json');
+
+// 获取目录下的图片数组
+function getImgs(input) {
+    if (!fs.existsSync(input)) {
+        console.warn('路径不存在');
+        return [];
+    }
+    const files = fs.readdirSync(input);
+    const imgs = [];
+    for (let index = 0; index < files.length; index++) {
+        const file = files[index];
+        if (file.indexOf('png') !== -1 && file.indexOf('sprite') === -1) {
+            imgs.push(path.resolve(input, file));
+        }
+    }
+    return imgs;
+}
+
+// 生成雪碧图
+function createSprite(input, output, mode) {
+    const imgs = getImgs(input);
+    if (imgs.length === 0) {
+        return console.warn('无可用图片资源');
+    }
+    const img = nodeImages(imgs[0]);
+    const width = img.width();
+    const height = img.height();
+    const data = [];
+    let cw = 0, ch = 0;
+    const l = getCol(imgs.length);
+    const row = getRow(imgs.length, l);
+    
+    switch (mode) {
+        case 0:
+            cw = width;
+            ch = height * imgs.length;
+            for (let index = 0; index < imgs.length; index++) {
+                const img = imgs[index];
+                data.push({
+                    img: nodeImages(img),
+                    x: 0,
+                    y: index * height
+                })
+            }
+            break;
+        case 1:
+            cw = width * imgs.length;
+            ch = height;
+            for (let index = 0; index < imgs.length; index++) {
+                const img = imgs[index];
+                data.push({
+                    img: nodeImages(img),
+                    x: index * width,
+                    y: 0
+                })
+            }
+            break;
+        case 2:
+            cw = width * l;
+            ch = height * row;
+            for (let index = 0; index < imgs.length; index++) {
+                const img = imgs[index];
+                const i = index % l;
+                const x = i * width;
+                const y = parseInt(index / l) * height
+                data.push({
+                    img: nodeImages(img),
+                    x: x,
+                    y: y
+                })
+            }
+            break;
+        case 3:
+            cw = width * row;
+            ch = height * l;
+            for (let index = 0; index < imgs.length; index++) {
+                const img = imgs[index];
+                const i = index % l;
+                const x = parseInt(index / l) * width;
+                const y = i * height;
+                data.push({
+                    img: nodeImages(img),
+                    x: x,
+                    y: y
+                })
+            }
+            break;
+    }
+    
+    const canvas = nodeImages(cw, ch);
+    for (let index = 0; index < data.length; index++) {
+        const item = data[index];
+        canvas.draw(item.img, item.x, item.y);
+    }
+    canvas.saveAsync(output + '/sprite.png', function() {
+        console.log('雪碧图已生成');
+        createCss(output, data, mode);
+    });
+}
+
+// 雪碧图几行显示
+function getCol(nums) {
+    const i = 9;
+    let l = 1;
+    for (let index = 1; index < i; index++) {
+        if (index * index - nums >= 0) {
+            l = index;
+            return l;
+        }
+    }
+    return l;
+}
+
+function getRow(nums, col) {
+    const n = nums % col
+    const row = Math.floor(nums / col)
+    if (n > 0) return row + 1
+    return row
+}
+
+// 生成css文件
+function createCss(out, data, mode) {
+    let tpl = '@keyframes frame-sprite {';
+
+    const guld = (100 / (data.length-1)).toFixed(2)
+
+    for (let index = 0; index < data.length; index++) {
+        const item = data[index];
+        if (mode === 0) {
+            tpl += '\r\r\n   '+(index * guld)+'% {\r\n        background-position: ' + item.x + 'px -' + item.y + 'px;\r\n    }';
+        } else if (mode === 1) {
+            tpl += '\r\r\n   '+(index * guld)+'% {\r\n        background-position: -' + item.x + 'px ' + item.y + 'px;\r\n    }';
+        } else if (mode === 2) {
+            tpl += '\r\r\n   '+(index * guld)+'% {\r\n        background-position: -' + item.x + 'px -' + item.y + 'px;\r\n    }';
+        } else if (mode === 3) {
+            tpl += '\r\r\n   '+(index * guld)+'% {\r\n        background-position: -' + item.x + 'px -' + item.y + 'px;\r\n    }';
+        } else {
+
+        };
+    }
+
+    tpl += '\r\n}';
+
+    fs.writeFile(out + '/sprite.less', tpl, function (err) {
+        if (err) {
+            return console.error(err);
+        }
+        console.log("less已生成");
+    });
+}
+
+// 开始
+function start(input, output, model) {
+    createSprite(input, output, model);
+}
+
+// 命令行
+program
+    .version(config.version, '-v, --version')
+    .option('-m, --model <n>', 'sprite models', parseInt, 0)
+    .option('-o, --output <string>', 'output directory', null, 'img')
+    .option('-i, --input <string>', 'input directory', null, 'img')
+    .parse(process.argv);
+
+if (!program.output) {
+    program.output = program.input;
+}
+
+start(program.input, program.output, program.model);
